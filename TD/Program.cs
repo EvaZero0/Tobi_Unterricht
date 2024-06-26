ea7bca43dea32cec921b3ce9a5082c3a48c1f002
EvaZero0 <burnitdown89@hotmail.com>
2024-06-26 09:30:31 +0200

    eiskalt kopiert

---
+++ TD/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Program-Einstiegspunkt C#
+* namespace TD
+{
+ // Programmeinstiegspunkt (Wo das Programm anfängt auszuführen.)
+ // => Program.Main
+ internal class Program
+ {
+   static void Main(string[] args)
+   {
+     Console.WriteLine("Hello, World!");
+     Console.ReadLine();
+     // hier...
+     var x = Start();
+     Console.WriteLine(x);
+   }
+   static string Start()
+   {
+     Console.WriteLine("Buxtehude!");
+     return "blubb";
+   }
+   // const login = function() {...}
+   // login()
+ }
+}
+*/
+
+// Menü anzeigen mit Optionen
+Console.WriteLine("Willkommen bei TD!");
+Console.WriteLine("Was möchtest du tun?");
+Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen");
+
+do
+{
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            // Option 1: Todo erstellen
+            // Userintput nehmen
+            var todo = Console.ReadLine();
+
+            // Neues Todo zur Todoliste hinzufügen
+            // Todo erstellen
+            Console.WriteLine("CREATE");
+            break;
+        case "2":
+            // Liste der Todos anzeigen
+            Console.WriteLine("READ");
+            break;
+    }
+} while (false);
