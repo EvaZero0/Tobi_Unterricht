de1b1b8913374b94ab4fb8d9ddeecba3f9ac1f52
EvaZero0 <168176416+EvaZero0@users.noreply.github.com>
2024-06-26 13:12:25 +0200

    neue version

--- TD/Program.cs
+++ TD/Program.cs
@@ -26,29 +26,63 @@
 
 // Menü anzeigen mit Optionen
 Console.WriteLine("Willkommen bei TD!");
-Console.WriteLine("Was möchtest du tun?");
-string[] todos
+
+string[] todos = new string[10];
+
+// das hier ist aktuell noch null!!!
+// Console.WriteLine(todos[9]);
 
 do
 {
-    Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen");
+    Console.Clear();
+    Console.WriteLine("Was möchtest du tun?");
+    Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen\n\t3. Todo abhacken");
+    var input = Console.ReadLine();
 
-var input = Console.ReadLine();
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("Was hast du zu tun? <Enter zum bestätigen>");
+            var todo = Console.ReadLine();
+            // Iterieren durch alle Todos (auch die leeren)
+            for (int i = 0; i < todos.Length; i++)
+            {
+                // Wenn das aktuelle Element im Array leer ist, dann...
+                if (todos[i] == null)
+                {
+                    // Fügen wir das Todo hinzu
+                    todos[i] = todo;
+                    Console.WriteLine("Todo hinzugefügt: " + todos[i]);
+                    break;
+                }
+            }
 
-switch (input)
-{
-    case "1":
-        // Option 1: Todo erstellen
-        // Userintput nehmen
-        var todo = Console.ReadLine();
-
-        // Neues Todo zur Todoliste hinzufügen
-        // Todo erstellen
-        Console.WriteLine("CREATE");
-        break;
-    case "2":
-        // Liste der Todos anzeigen
-        Console.WriteLine("READ");
-        break;
-}
-} while (false) ;
+            break;
+        case "2":
+            Console.WriteLine("Du hast folgende Aufgaben:");
+            for (int i = 0; i < todos.Length; i++)
+            {
+                Console.WriteLine($"\t{i}. {todos[i]}");
+            }
+            Console.WriteLine("Drücke <ENTER> um zurück ins Menü zu kommen.");
+            Console.ReadKey();
+            break;
+        case "3":
+            // Löschen
+            Console.WriteLine("Welches Todo möchtest du löschen?");
+            for (int i = 0; i < todos.Length; i++)
+            {
+                Console.WriteLine($"\t{i}. {todos[i]}");
+            }
+            var todoIndexString = Console.ReadLine();
+            int todoIndex = int.Parse(todoIndexString);
+            // float todoIndex = float.Parse(todoIndexString);
+            // int todoIndex1 = Convert.ToInt32(todoIndexString);
+            todos[todoIndex] = null;
+            break;
+        case "4":
+            // Updaten/Verändern eines Todos (verändern des strings)
+            // 
+            break;
+    }
+} while (true);
